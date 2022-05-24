893257f4331095b729933d51cd2288a1bd561925
Fredy Lucho <fredy.learning@gmail.com>
2022-05-24 01:52:00 -0500

    Version 2.2.2.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,10 +27,38 @@ function scrollActive(){
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.c-nav-menu a[href*=' + sectionId + ']').classList.add('c-active-link')
+            document.querySelector('.c-container-menu a[href*=' + sectionId + ']').classList.add('c-active-link')
         }else{
-            document.querySelector('.c-nav-menu a[href*=' + sectionId + ']').classList.remove('c-active-link')
+            document.querySelector('.c-container-menu a[href*=' + sectionId + ']').classList.remove('c-active-link')
         }
     })
 }
 window.addEventListener('scroll', scrollActive)
+
+/*==================== ADD BOX-SHADOW MENU ====================*/ 
+function scrollNav(){
+    const nav = document.getElementById('i-menu')
+    if(this.scrollY >= 10) nav.classList.add('c-scroll-nav'); else nav.classList.remove('c-scroll-nav')
+}
+window.addEventListener('scroll', scrollNav)
+
+/*==================== SHOW SCROLLTOP ====================*/ 
+function scrollTop(){
+    const scrollTop = document.getElementById('i-scrolltop');
+    if(this.scrollY >= 300) scrollTop.classList.add('c-show-scroll'); 
+    else scrollTop.classList.remove('c-show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
+/*==================== SWIPER PORTAFOLIO ====================*/ 
+let swiper = new Swiper('.swiper-container', {
+    cssMode: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    }
+});
